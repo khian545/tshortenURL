1d8401bd57ae88e7937954fa0010347c30e834c9
khian545 <deguzmanjakeb@gmail.com>
2020-04-11 00:02:08 +0800

    Updated generate.php

--- generate.php
+++ generate.php
@@ -1,13 +1,31 @@
 <?php
 include "header.php";
+?>
 
-include 'URLShortener.php';
-$shortener = new URLShortener();
-$shortener->data["destination"] = $_GET["destination"];
-echo '...Generating...';
-echo '<br/>';
-$shortener->generateShortURL();
-echo '<br/>'; echo '<br/>';
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <?php
+        include 'URLShortener.php';
+        $shortener = new URLShortener();
+        $shortener->data["destination"] = $_GET["destination"];
+        echo '...Generating...';
+        echo '<br/>';
+        $shortener->generateShortURL();
+        echo '<br/>'; echo '<br/>';
+      ?>
 
+      <form action="index.php" method="get">
+        <div class="form-group">
+          <button class="btn btn-primary" type="submit">Back</button>
+        </div>
+      </form>
+
+    </div>
+  </div>
+
+</div>
+
+<?php
 include "footer.php";
  ?>
